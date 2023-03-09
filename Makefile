ee9fbeee4c7efd454b37ab0f0ad5f665f8027061
laura desire <laura.desire98@gmail.com>
2023-03-09 13:02:32 +0100

    création de registry, nouvelle fonction sur Makefile, rajout de mlflow sur requirement

--- Makefile
+++ Makefile
@@ -8,3 +8,11 @@ preprocess_data:
 
 augment_data:
 	@python -c 'from ddd.ml_logic.preprocess import augment_pictures; augment_pictures()'
+
+
+#setting directory for saving parameters localy
+create_local_files:
+	@mkdir training_outputs
+	@mkdir training_outputs/metrics
+	@mkdir training_outputs/models
+	@mkdir training_outputs/params
--- ddd/params.py
+++ ddd/params.py
@@ -1,6 +1,8 @@
 import os
 
 IMAGE_SIZE = 256
+MODEL_TARGET = os.environ.get('MODEL_TARGET')
+
 
 GCP_PROJECT = os.environ.get("GCP_PROJECT")
 GCS_DATA_BUCKET = os.environ.get("GCS_DATA_BUCKET")
@@ -21,3 +23,10 @@ VIRUSES = [
 ]
 
 IMAGES_PER_VIRUS = 736  #target number of imagettes for augmentation
+
+
+LOCAL_REGISTRY_PATH= os.environ.get('LOCAL_REGISTRY_PATH')
+
+MLFLOW_TRACKING_URI = os.environ.get("MLFLOW_TRACKING_URI")
+MLFLOW_EXPERIMENT = os.environ.get("MLFLOW_EXPERIMENT")
+MLFLOW_MODEL_NAME = os.environ.get("MLFLOW_MODEL_NAME")
