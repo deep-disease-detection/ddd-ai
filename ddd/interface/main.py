f93ef193433afa7bee7a11681e07f9953a73791a
laura desire <laura.desire98@gmail.com>
2023-03-09 11:39:04 +0100

    fontion get_dataset

--- ddd/interface/main.py
+++ ddd/interface/main.py
@@ -1,2 +1,48 @@
 from google.cloud import storage
 from ddd.params import *
+import os
+
+def get_dataset():
+    '''
+    Return processed train, validation and test dataset from directory
+    '''
+
+    from tensorflow.keras.utils import image_dataset_from_directory
+
+    print("Getting train dataset :)")
+    train = image_dataset_from_directory(
+        AUGTRAIN_PATH,
+        labels='inferred',
+        label_mode='categorical',
+        image_size=IMAGE_SIZE
+    )
+
+    print('Getting validation dataset :)')
+    validation = image_dataset_from_directory(
+        VALIDATION_PATH,
+        labels='inferred',
+        label_mode='categorical',
+        image_size= IMAGE_SIZE
+    )
+
+    print('Getting test dataset :)')
+    test = image_dataset_from_directory(
+        TEST_PATH,
+        labels='inferred',
+        label_mode='categorical',
+        image_size= IMAGE_SIZE
+    )
+
+    print('All done !✅')
+    return train, validation, test
+
+
+
+def train_model(nom_model):
+    '''
+    Download processed dataset from local directory
+    Train the model on the processed dataset
+    Store training result
+    '''
+
+    train, val, test = get_dataset()
