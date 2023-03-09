4583c4b90cbcc8f126e7cc4caebce544983e7375
Youssef El Ouard <youselouard@gmail.com>
2023-03-09 08:09:50 +0100

    Added new parameters to params file

--- ddd/params.py
+++ ddd/params.py
@@ -7,9 +7,17 @@ GCS_DATA_BUCKET = os.environ.get("GCS_DATA_BUCKET")
 
 RAW_DATA_PATH = os.environ.get("RAW_DATA_PATH")
 PROCESS_DATA_PATH = os.environ.get("PROCESS_DATA_PATH")
+TO_PREPROCESS = os.environ.get("TO_PREPROCESS")
+
+AUGTRAIN_PATH = os.environ.get("AUGTRAIN_PATH")
+TRAIN_PATH = os.path.join(PROCESS_DATA_PATH, 'train')
+VALIDATION_PATH = os.path.join(PROCESS_DATA_PATH, 'validation')
+TEST_PATH = os.path.join(PROCESS_DATA_PATH, 'test')
 
 VIRUSES = [
     'Adenovirus', 'Astrovirus', 'CCHF', 'Cowpox', 'Ebola', 'Influenza',
     'Lassa', 'Marburg', 'Nipah virus', 'Norovirus', 'Orf', 'Papilloma',
     'Rift Valley', 'Rotavirus'
 ]
+
+IMAGES_PER_VIRUS = 736  #target number of imagettes for augmentation
